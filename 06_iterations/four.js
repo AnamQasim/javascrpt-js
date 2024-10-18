a37a71df96a7f09d6fa6055497c13f18cf23b27a
Anam Qasim <anamqasimearnandlive@gmail.com>
2024-10-18 13:03:24 +0000

    coder

---
+++ 06_iterations/four.js
@@ -0,0 +1,31 @@
+//const myobject = {
+    //js: 'javascript',
+    //cpp: 'C++',
+    //rb: 'ruby',
+    //swift: 'swift by apple'
+//}
+
+//for (const key in myobject) {
+    //console.log(myobject[key]);
+    //console.log(`${key} shortcut is for ${myobject[key]}`);            
+//}
+
+
+//arrays in forin loop
+const programming = ["js", "rb", "py", "java"]
+for (const key in programming) {
+    //console.log(key);
+    //console.log(programming[key]);    
+}
+
+
+//forin loop maps does not work as maps are not irritable
+const map = new Map()
+map.set['IN, "India']
+map.set['USA, "United States of America']
+map.set['Fr, "France']
+map.set['IN, "India']
+
+//for (const key in map) {
+    //console.log(key);    
+//}
--- 06_iterations/three.js
+++ 06_iterations/three.js
@@ -9,12 +9,12 @@
 //    console.log(num);    
 //}
 
-const greetings = "Hello world!"
+//const greetings = "Hello world!"
 
-for (const greet of greetings) {
-    console.log(`each char is ${greet}`);
+//for (const greet of greetings) {
+    //console.log(`each char is ${greet}`);
     
-}
+//}
 
 //MAPS
 
@@ -22,3 +22,21 @@ const map = new Map()
 map.set['IN, "India']
 map.set['USA, "United States of America']
 map.set['Fr, "France']
+map.set['IN, "India']
+
+//console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);         
+}
+
+
+const myobj = {
+    'Game1': 'NFS',
+    'Game2': 'Spiderman'
+}
+
+//object not happens in forloop
+//for (const [key, value] of myobj) {
+//    console.log(key, ':-', value);    
+//}
