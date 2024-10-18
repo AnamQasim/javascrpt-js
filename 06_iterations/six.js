b2b3b17c6f2b9d4328b54b3ea96aa239ea1e11fd
Anam Qasim <anamqasimearnandlive@gmail.com>
2024-10-18 15:48:12 +0000

    six

---
+++ 06_iterations/six.js
@@ -0,0 +1,38 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+ //will not return in foreach
+//const values = coding.forEach( (item) => {
+//    console.log(item);
+//    return item
+    
+//} )
+
+
+
+
+
+
+//basics of filter
+//filter inside call back action then condition and if that condition meets then numbers will show
+//const myNums = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10]
+//if you start scope {} then return keyword you need to write like return num
+//const newNums = myNums.filter( (num) => num > 4)
+//console.log(newNums);
+
+//filter or you can use this foreach return
+
+//foreach return like this
+//const newNums = []
+
+//myNums.forEach( (num) => {
+//    if (num > 4) {
+//        newNums.push(num)
+//    }
+//} )
+//console.log(newNums);
+
+
+
+
+
+
